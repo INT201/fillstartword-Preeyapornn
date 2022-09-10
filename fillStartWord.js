0e30ee7c12eeebd8e7b144dd8c5443a60f78ed93
Preeyaporn Petcharat <preeyaporn.benz@mail.kmutt.ac.th>
2022-09-10 17:56:25 +0700

    final1

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,11 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  //code here
+if ( word === null || word === undefined){
+    return undefined
+} else if ( word.startsWith(startWord)){
+    return word
+}
+    return startWord.concat(word);
 }
 module.exports = fillStartWord
